3caf5c2889df9a04a06d37cee521ed4e6fe620df
TanninOne <sherb@gmx.net>
2018-02-22 13:24:36 +0100

    added documentation

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ API used by Vortex extensions.
 
 Technically you don't need this module. When your extension run (regularly), vortex will inject the vortex-api module, so this module is mainly relevant as a reference and to provide command completion in an IDE.
 
+# API Reference
+
+The API reference is available at https://Nexus-Mods.github.io/vortex-api.
+However, for technical reasons this reference may currently contain classes and interfaces that are not actually exposed through the api.
---
+++ typedoc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "mode": "file",
+  "out": "docs",
+  "theme": "default",
+  "ignoreCompilerErrors": "true",
+  "experimentalDecorators": "true",
+  "emitDecoratorMetadata": "true",
+  "target": "ES6",
+  "moduleResolution": "node",
+  "preserveConstEnums": "true",
+  "stripInternal": "true",
+  "suppressExcessPropertyErrors": "true",
+  "suppressImplicitAnyIndexErrors": "true",
+  "externalPattern": "**/node_modules/**",
+  "exclude": "**/__tests__/**",
+  "_excludeExternals": "true",
+  "_excludePrivate": "true",
+  "module": "commonjs",
+  "readme": "./README.md",
+  "tsconfig": "./tsconfig.json"
+}
