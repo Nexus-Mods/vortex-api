a6e0f87579f776907912f4459f5719a2311e93a3
TanninOne <seppleviathan@gmx.de>
2018-10-01 14:21:05 +0200

    got rid of unneeded source files

---
+++ lib/extensions/mod_management/actions/deployment.d.ts
@@ -0,0 +1,4 @@
+export declare const setDeploymentNecessary: import("../../../../../../Work/Vortex/node_modules/redux-act").ComplexActionCreator2<string, boolean, {
+    gameId: string;
+    required: boolean;
+}, {}>;
