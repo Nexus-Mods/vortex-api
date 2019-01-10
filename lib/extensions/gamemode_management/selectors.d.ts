03b2164b471b786977277c966563c105466f5a49
TanninOne <seppleviathan@gmx.de>
2019-01-10 11:03:58 +0100

    another signature fix

--- lib/extensions/gamemode_management/selectors.d.ts
+++ lib/extensions/gamemode_management/selectors.d.ts
@@ -1,18 +1,20 @@
 import { IState } from '../../types/IState';
 import { IDiscoveryResult } from './types/IDiscoveryResult';
 import { IGameStored } from './types/IGameStored';
+import { OutputSelector } from 'reselect';
+import { ICacheObject, OutputParametricSelector, ParametricSelector } from 're-reselect';
 export declare function knownGames(state: any): IGameStored[];
-export declare const currentGame: import("../../../../../Work/Vortex/node_modules/reselect").OutputSelector<any, IGameStored, (res1: IGameStored[], res2: string) => IGameStored>;
-export declare const gameById: import("../../../../../Work/Vortex/node_modules/re-reselect").ParametricSelector<any, string, IGameStored> & {
+export declare const currentGame: OutputSelector<any, IGameStored, (res1: IGameStored[], res2: string) => IGameStored>;
+export declare const gameById: ParametricSelector<any, string, IGameStored> & {
     resultFunc: (res1: IGameStored[], res2: string) => IGameStored;
     recomputations: () => number;
     resetRecomputations: () => number;
 } & {
-    getMatchingSelector: (state: any, props: string, ...args: any[]) => import("../../../../../Work/Vortex/node_modules/re-reselect").OutputParametricSelector<any, string, IGameStored, (res1: IGameStored[], res2: string) => IGameStored>;
+    getMatchingSelector: (state: any, props: string, ...args: any[]) => OutputParametricSelector<any, string, IGameStored, (res1: IGameStored[], res2: string) => IGameStored>;
     removeMatchingSelector: (state: any, props: string, ...args: any[]) => void;
     clearCache: () => void;
     resultFunc: (res1: IGameStored[], res2: string) => IGameStored;
-    cache: import("../../../../../Work/Vortex/node_modules/re-reselect").ICacheObject;
+    cache: ICacheObject;
 };
 /**
  * return the discovery information about a game
@@ -22,14 +24,14 @@ export declare const gameById: import("../../../../../Work/Vortex/node_modules/r
  * @returns {IDiscoveryResult}
  */
 export declare function currentGameDiscovery(state: any): IDiscoveryResult;
-export declare const discoveryByGame: import("../../../../../Work/Vortex/node_modules/re-reselect").ParametricSelector<IState, string, IDiscoveryResult> & {
+export declare const discoveryByGame: ParametricSelector<IState, string, IDiscoveryResult> & {
     resultFunc: (res1: {
         [id: string]: IDiscoveryResult;
     }, res2: string) => IDiscoveryResult;
     recomputations: () => number;
     resetRecomputations: () => number;
 } & {
-    getMatchingSelector: (state: IState, props: string, ...args: any[]) => import("../../../../../Work/Vortex/node_modules/re-reselect").OutputParametricSelector<IState, string, IDiscoveryResult, (res1: {
+    getMatchingSelector: (state: IState, props: string, ...args: any[]) => OutputParametricSelector<IState, string, IDiscoveryResult, (res1: {
         [id: string]: IDiscoveryResult;
     }, res2: string) => IDiscoveryResult>;
     removeMatchingSelector: (state: IState, props: string, ...args: any[]) => void;
@@ -37,6 +39,6 @@ export declare const discoveryByGame: import("../../../../../Work/Vortex/node_mo
     resultFunc: (res1: {
         [id: string]: IDiscoveryResult;
     }, res2: string) => IDiscoveryResult;
-    cache: import("../../../../../Work/Vortex/node_modules/re-reselect").ICacheObject;
+    cache: ICacheObject;
 };
 export declare function gameName(state: any, gameId: string): string;
