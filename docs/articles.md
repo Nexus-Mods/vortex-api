091c4d8992e11f6662ff01d6ef5544a54d869e44
TanninOne <seppleviathan@gmx.de>
2020-10-22 16:11:01 +0200

    further testing

--- docs/articles.md
+++ docs/articles.md
@@ -1,7 +1,11 @@
+---
+layout: default
+title: Articles
+---
 # Articles
 ## UI
-[UI](https://github.com/Nexus-Mods/vortex-api/issues/6) (WIP) by TanninOne (Last update: Thu Oct 22 2020 15:40:24 GMT+0200 (GMT+02:00))
+[UI](articles/UI.md) (WIP) by TanninOne (Last update: Thu Oct 22 2020 15:40:24 GMT+0200 (GMT+02:00))
 
 ## General
-[Introduction](https://github.com/Nexus-Mods/vortex-api/issues/5) (WIP) by TanninOne (Last update: Thu Oct 22 2020 15:40:31 GMT+0200 (GMT+02:00))
+[Introduction](articles/Introduction.md) (WIP) by TanninOne (Last update: Thu Oct 22 2020 15:40:31 GMT+0200 (GMT+02:00))
 
--- update.js
+++ update.js
@@ -39,12 +39,12 @@ function makeTOC(articles) {
     return prev;
   }, {});
 
-  let res = '# Articles\n';
+  let res = `---\nlayout: default\ntitle: Articles\n---\n` + '# Articles\n';
 
   Object.keys(groups).forEach(group => {
     res += `## ${group}\n`
         + groups[group].map(article =>
-          `[${article.title}](${article.html_url})${state(article)} by ${article.user.login} (Last update: ${time(article)})`)
+          `[${article.title}](articles/${article.title}.md)${state(article)} by ${article.user.login} (Last update: ${time(article)})`)
           .join('\n')
         + '\n\n';
   });
@@ -52,11 +52,28 @@ function makeTOC(articles) {
   return res;
 }
 
+function makeNav(articles) {
+  return '<nav>'
+    + articles.map(article => `<a href="articles/${article.title}.md">${article.title}</a>`).join('<br/>')
+    + '</nav>';
+}
+
+function articleFrame(article) {
+  return `---\nlayout: default\ntitle: ${article.title}\n---\n`
+    + article.body
+    + '\n\n'
+    + `[Discuss this article](${article.html_url})`;
+}
+
 async function main() {
   const articles = await req('https://api.github.com/repos/Nexus-Mods/vortex-api/issues?labels=Article', {
     labels: 'Article',
   });
   await fs.writeFile(path.join('docs', 'articles.md'), makeTOC(articles));
+  await fs.writeFile(path.join('docs', '_includes/navigation.html'), makeNav(articles));
+  for (const article of articles) {
+    await fs.writeFile(path.join('docs', 'articles', article.title + '.md'), articleFrame(article));
+  }
   console.log(articles);
 }
 
