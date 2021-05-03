89d870267200d89452a40e0f0f0fe6b752fd28c3
idcs <idc.dts@gmail.com>
2021-05-03 09:12:00 +0100

    Adding gamespecific types for modpacks

---
+++ lib/types/Collections/IGameSpecificInterfaceProps.d.ts
@@ -0,0 +1,7 @@
+import { IRevision } from '@nexusmods/nexus-api';
+import * as types from '../api';
+export interface IGameSpecificInterfaceProps {
+    t: types.TFunction;
+    collection: types.IMod;
+    revisionInfo: IRevision;
+}
---
+++ lib/types/Collections/api.d.ts
@@ -0,0 +1,5 @@
+import { ICollectionsGameSupportEntry } from './ICollectionsGameSupportEntry';
+import { IGameSpecificGeneratorProps } from './IGameSpecificGeneratorProps';
+import { IGameSpecificInterfaceProps } from './IGameSpecificInterfaceProps';
+import { IGameSpecificParserProps } from './IGameSpecificParserProps';
+export { ICollectionsGameSupportEntry, IGameSpecificGeneratorProps, IGameSpecificInterfaceProps, IGameSpecificParserProps, };
