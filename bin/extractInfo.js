5f43727201db8b498f03c7e75b703a379864bc9b
Alistair Chapman <alistair@agchapman.com>
2021-05-27 14:22:34 +1000

    Refactor name extraction (again)

--- bin/extractInfo.js
+++ bin/extractInfo.js
@@ -7,6 +7,16 @@ function transformName(input) {
   return input.charAt(0).toUpperCase() + input.substr(1).replace(/-([a-z])/g, (match, m1) => ' ' + m1.toUpperCase());
 }
 
+function getExtensionName(pkgInfo) {
+  if (pkgInfo.config && pkgInfo.config.game) {
+    return `Game: ${pkgInfo.config.game}`;
+  }
+  if (pkgInfo.config && pkgInfo.config.extensionName) {
+    return pkgInfo.config.extensionName;
+  }
+  return transformName(pkgInfo.name);
+}
+
 /**
  * function intended to be run during build of an extension,
  * extracting details about it from its package.json
@@ -17,7 +27,7 @@ function extractExtensionInfo(extPath) {
   const pkgInfo = JSON.parse(fs.readFileSync(path.join(extPath, 'package.json')).toString());
 
   return {
-    name: (pkgInfo.config && pkgInfo.config.game) ? `Game: ${pkgInfo.config.game}` : transformName(pkgInfo.name),
+    name: getExtensionName(pkgInfo),
     namespace: (pkgInfo.config && pkgInfo.config.namespace) ? pkgInfo.config.namespace : undefined,
     author: pkgInfo.author,
     version: pkgInfo.version,
