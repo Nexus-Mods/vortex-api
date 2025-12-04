003a1522855e859901482004db89aa1fd569bda5
IDCs <adrianraer@gmail.com>
2025-12-04 10:12:40 +0000

    updating api

--- lib/extensions/category_management/util/retrieveCategoryPath.d.ts
+++ lib/extensions/category_management/util/retrieveCategoryPath.d.ts
@@ -5,7 +5,7 @@ import { IState } from '../../../types/IState';
  * @param {number} category
  * @param {Redux.Store<any>} store
  */
-export declare function resolveCategoryPath(category: string, state: IState): string;
+export declare function resolveCategoryPath(category: string | number, state: IState): string;
 export declare function resolveCategoryId(name: string, state: IState): number;
 /**
  * retrieve the Category from the Store
@@ -13,4 +13,4 @@ export declare function resolveCategoryId(name: string, state: IState): number;
  * @param {number} category
  * @param {Redux.Store<any>} store
  */
-export declare function resolveCategoryName(category: string, state: IState): string;
+export declare function resolveCategoryName(category: string | number, state: IState): string;
