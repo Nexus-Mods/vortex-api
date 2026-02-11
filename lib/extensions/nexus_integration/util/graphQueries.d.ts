93c1c8bf365f4379401fe4c6ad8233f1c189cd41
Vitalii Mikhailov <personal@aragas.org>
2026-02-11 18:41:57 +0200

    Added collection stuff

--- lib/extensions/nexus_integration/util/graphQueries.d.ts
+++ lib/extensions/nexus_integration/util/graphQueries.d.ts
@@ -3,3 +3,4 @@ export declare const FULL_COLLECTION_INFO: ICollectionQuery;
 export declare const FULL_REVISION_INFO: IRevisionQuery;
 export declare const CURRENT_REVISION_INFO: ICollectionQuery;
 export declare const COLLECTION_SEARCH_QUERY: ICollectionQuery;
+export declare const MY_COLLECTIONS_SEARCH_QUERY: ICollectionQuery;
